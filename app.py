c9843aa6fb1b04eb023ee7e8db9b0b6bb2a68f29
Ari <arisaitellez@hotmail.com>
2026-01-17 19:30:19 -0600

    clean

--- app.py
+++ app.py
@@ -7,13 +7,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask("Login")
-# Esta llave encripta la cookie para que nadie pueda falsificar su sesión
+
 app.secret_key = os.getenv('FLASK_SECRET_KEY', 'clave_default_solo_para_local')
 
 
-# --- CONFIGURACIÓN DE CONEXIÓN ---
 def get_connection_string(user, pwd):
-    """Genera el texto de conexión para SQL Server"""
+
     server = os.getenv('DB_SERVER')
     database = os.getenv('DB_NAME')
     driver = '{ODBC Driver 18 for SQL Server}'
@@ -34,16 +33,12 @@ def login():
 
     connection = None
     try:
-        # 1. PRUEBA DE FUEGO: Intentamos conectar con lo que escribió el usuario.
-        # Si la contraseña es incorrecta, pyodbc dará error y saltará al 'except'.
+
         conn_str = get_connection_string(username, password)
         connection = pyodbc.connect(conn_str)
 
-        # Si la línea de arriba no falló, ¡es el usuario correcto!
         session['username'] = username
 
-        # NOTA IMPORTANTE: Ya NO guardamos session['password'].
-        # Esto elimina la vulnerabilidad crítica de exponer credenciales en cookies.
 
         return redirect(url_for('tables'))
 
@@ -57,7 +52,7 @@ def login():
 
 @app.route('/tables', methods=['GET', 'POST'])
 def tables():
-    # Si no se ha logueado, va para afuera
+    #verificar log
     if 'username' not in session:
         return redirect(url_for('home'))
 
@@ -65,17 +60,14 @@ def tables():
     cursor = None
 
     try:
-        # TRUCO PARA EL PROYECTO:
-        # Como ya validamos que el usuario es real en el Login, ahora usamos
-        # la "Cuenta Maestra" (del .env) para mostrarle los datos.
-        # Esto evita tener que guardar la contraseña del alumno en la sesión.
+
         sys_user = os.getenv('DB_USER')
         sys_pwd = os.getenv('DB_PASSWORD')
 
         connection = pyodbc.connect(get_connection_string(sys_user, sys_pwd))
         cursor = connection.cursor()
 
-        # 1. Obtener lista de tablas (Lista Blanca para evitar Hackeos)
+        # Obtener lista de tablas
         cursor.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'")
         tables_list = [row[0] for row in cursor.fetchall()]
 
@@ -84,7 +76,7 @@ def tables():
         if request.method == 'POST':
             selected_table = request.form['table_name']
 
-            # 2. VALIDACIÓN DE SEGURIDAD (Anti-SQL Injection)
+            #VALIDACIÓN DE SEGURIDAD
             if selected_table in tables_list:
                 cursor.execute(f"SELECT * FROM [{selected_table}]")
                 # Obtenemos los nombres de las columnas para que la tabla se vea bonita
